f75fa6ffb8409c8c4fd2c3dc2b210264fd899f0c
manzor99 <manzor99@gmail.com>
2017-04-20 13:46:48 -0400

    Test and Menu Bar

--- src/testCases/PersonTest.java
+++ src/testCases/PersonTest.java
@@ -4,51 +4,68 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import people.Person;
+import people.Buyer;
+
 public class PersonTest {
 
 	@Test
-	public void testPerson() {
-		fail("Not yet implemented");
-	}
-
-	@Test
 	public void testGetIdNumber() {
-		fail("Not yet implemented");
+		Buyer person = new Buyer(1, "Rocco", "none@none.net", "notification");
+		
+		assertEquals(1, person.getIdNumber());
 	}
 
 	@Test
 	public void testSetIdNumber() {
-		fail("Not yet implemented");
+		Buyer person = new Buyer(1, "Rocco", "none@none.net", "notification");
+		person.setIdNumber(7);
+		
+		assertEquals(7, person.getIdNumber());
 	}
 
 	@Test
 	public void testGetName() {
-		fail("Not yet implemented");
+		Buyer person = new Buyer(1, "Rocco", "none@none.net", "notification");
+		
+		assertEquals("Rocco", person.getName());
 	}
 
 	@Test
 	public void testSetName() {
-		fail("Not yet implemented");
+		Buyer person = new Buyer(1, "Rocco", "none@none.net", "notification");
+		person.setName("Ryan");
+		
+		assertEquals("Ryan", person.getIdNumber());
 	}
 
 	@Test
 	public void testGetEmail() {
-		fail("Not yet implemented");
+		Buyer person = new Buyer(1, "Rocco", "none@none.net", "notification");
+		
+		assertEquals("none@none.net", person.getEmail());
 	}
 
 	@Test
 	public void testSetEmail() {
-		fail("Not yet implemented");
+		Buyer person = new Buyer(1, "Rocco", "none@none.net", "notification");
+		person.setEmail("email");
+		
+		assertEquals("email", person.getEmail());
 	}
 
 	@Test
 	public void testGetNotification() {
-		fail("Not yet implemented");
+		Buyer person = new Buyer(1, "Rocco", "none@none.net", "notification");
+		
+		assertEquals("notification", person.getNotification());
 	}
 
 	@Test
 	public void testSetNotification() {
-		fail("Not yet implemented");
+		Buyer person = new Buyer(1, "Rocco", "none@none.net", "notification");
+		person.setNotification("note");
+		assertEquals("note", person.getIdNumber());
 	}
 
 }
