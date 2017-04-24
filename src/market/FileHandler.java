8f8b3b1b2754fdc8b0fa3e5abd8e0adb980f6a4a
Andrew Kim <aykim17@gmail.com>
2017-04-24 12:06:24 -0400

    Finished FileHandler class

--- src/market/FileHandler.java
+++ src/market/FileHandler.java
@@ -13,99 +13,167 @@ public class FileHandler {
     private Scanner scanner;
     private FileWriter writer;
 
-    public FileHandler() {
+    public FileHandler() {  // Constructor for instantiating all files 
         buyers = new File("Buyers.csv");
         sellers = new File("Sellers.csv");
         inventory = new File("Inventory.csv");
     }
 
     /**
-     * Inserts Person into Buyer or Seller file
+     * Method to update Sellers
      *
-     * @param p The person to insert
+     * @param sellersList The list of sellers
      */
-    public void insertPerson(Person p) {
-        ArrayList<Product> list;
-
+    public void updateSellers(ArrayList<Seller> sellersList) {
         try {
-            if (p instanceof Buyer) {
-                writer = new FileWriter(buyers, true);
-                list = ((Buyer) p).getBoughtItems();
-            } else {
-                writer = new FileWriter(sellers, true);
-                list = ((Seller) p).getSellerInventory();
-            }
+            writer = new FileWriter(sellers);
+            writer.append("ID,Name,Email,Password,Inventory");  // Append the header
+            ArrayList<Product> list;
+            String toAppend;
 
-            writer.append(p.getIdNumber() + "," + p.getName() + "," + p.getEmail() + "," + list + "\n");
+            for (Seller s : sellersList) {
+                list = s.getSellerInventory();
+                toAppend = s.getIdNumber() + "," + s.getName() + "," + s.getEmail() + "," + s.getPassword() + "," + list;
+                writer.append("\n" + toAppend);
+            }
             writer.close();
         } catch (IOException e) {
         }
     }
 
     /**
-     * Inserts product into Inventory file
+     * Method to update Buyers
      *
-     * @param p The product to insert
+     * @param buyersList The list of buyers
      */
-    public void insertProduct(Product p) {
+    public void updateBuyers(ArrayList<Buyer> buyersList) {
         try {
-            writer = new FileWriter(inventory, true);
-            writer.append(p.getIDNumber() + "," + p.getName() + "," + p.getDescription()
-            + "," + p.getPrice() + "," + p.getQuantity() + "," + p.getSellerId());
+            writer = new FileWriter(buyers);
+            writer.append("ID,Name,Email,Password,Inventory");  // Append the header
+            ArrayList<Product> list;
+            String toAppend;
+
+            for (Buyer b : buyersList) {
+                list = b.getBoughtItems();
+                toAppend = b.getIdNumber() + "," + b.getName() + "," + b.getEmail() + "," + b.getPassword() + "," + list;
+                writer.append("\n" + toAppend);
+            }
+            
+            writer.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    /**
+     * Method to update Products file
+     *
+     * @param inventoryList The list of inventory
+     */
+    public void updateProducts(ArrayList<Product> inventoryList) {
+        try {
+            writer = new FileWriter(inventory);
+            writer.append("ID,Name,Description,Price,Quantity,SellerID");  // Append the header
+            String toAppend;
+
+            for (Product p : inventoryList) {
+                toAppend = "\n" + p.getIDNumber() + "," + p.getName() + "," + p.getDescription()
+                        + "," + p.getPrice() + "," + p.getQuantity() + "," + p.getSellerId();
+                writer.append("\n" + toAppend);
+            }
+            
+            writer.close();
         } catch (IOException e) {
         }
     }
 
     /**
-     * Updates Person in Buyer or Seller file
+     * Fill ArrayList from Inventory file
      *
-     * @param p The Person to update
-     * @param fieldNum The field to update
-     * @param newValue The new value
+     * @return The ArrayList version
      */
-    public void updatePerson(Person p, int fieldNum, String newValue) {
-        try {
-            String line;
-            
-            if (p instanceof Buyer) {
-                scanner = new Scanner(buyers);
-            } else {
-                scanner = new Scanner(sellers);
-            }
-            
-            while (scanner.nextLine() != null) {
-                line = scanner.nextLine();
-                System.out.println(line);
-                String[] row = line.split(",");
-                if (Integer.valueOf(row[0]) == p.getIdNumber()) {
-                    row[fieldNum] = newValue;
-                } else {
-                    scanner.nextLine();
-                }
-            }
-        } catch (IOException e) {
-        }
-    }
-
-    /**
-     * Updates Product in Inventory file
-     *
-     * @param p The product to update
-     * @param fieldNum The field to update
-     */
-    public void updateProduct(Person p, int fieldNum) {
-        
-    }
-
     public ArrayList<Product> fillInventory() {
-        return null;
+        ArrayList<Product> list = new ArrayList<>();
+
+        try {
+            scanner = new Scanner(inventory);
+            scanner.nextLine();
+            while (scanner.hasNextLine()) {
+                String[] line = scanner.nextLine().split(",");
+                list.add(new Product(Integer.valueOf(line[0]), Integer.valueOf(line[5]),
+                        Integer.valueOf(line[4]), line[1], line[2], Double.valueOf(line[3])));
+            }
+        } catch (FileNotFoundException | NumberFormatException e) {
+            System.out.println(e);
+        }
+        return list;
     }
 
+    /**
+     * Fill ArrayList from Buyers
+     *
+     * @return The ArrayList version
+     */
     public ArrayList<Buyer> fillBuyers() {
-        return null;
+        ArrayList<Buyer> list = new ArrayList<>();
+
+        try {
+            scanner = new Scanner(buyers);
+            scanner.nextLine();
+            while (scanner.hasNextLine()) {
+                String[] line = scanner.nextLine().trim().split(",");
+                list.add(new Buyer(Integer.valueOf(line[0]), line[1], line[2], line[3],
+                        stringToProduct(line[4])));
+            }
+        } catch (FileNotFoundException | NumberFormatException e) {
+            System.out.println(Arrays.toString(e.getStackTrace()));
+        }
+
+        return list;
     }
 
+    /**
+     * Fill ArrayList from Sellers
+     *
+     * @return The ArrayList version
+     */
     public ArrayList<Seller> fillSellers() {
-        return null;
+        ArrayList<Seller> list = new ArrayList<>();
+
+        try {
+            scanner = new Scanner(sellers);
+            while (scanner.hasNextLine()) {
+                scanner.nextLine();
+                String[] line = scanner.nextLine().split(",");
+                list.add(new Seller(Integer.valueOf(line[0]), line[1], line[2], line[3],
+                        stringToProduct(line[4])));
+            }
+        } catch (FileNotFoundException | NumberFormatException e) {
+        }
+
+        return list;
+    }
+
+    /**
+     * Method that parses String and turns it into ArrayList
+     *
+     * @param x The string
+     * @return The list version
+     */
+    private ArrayList<Product> stringToProduct(String x) {
+        ArrayList<Product> list = new ArrayList<>();
+        String newString = x.trim().substring(1, x.length() - 1);
+        String[] rows = newString.split(",");
+
+        if (!"[]".equals(x)) {  // If the inventory column isn't blank
+            for (String row : rows) {
+                String[] line = row.split(":");
+                list.add(new Product(Integer.valueOf(line[0]),
+                        Integer.valueOf(line[5]), Integer.valueOf(line[4]),
+                        line[1], line[2], Double.valueOf(line[3])));
+            }
+        }
+
+        return list;
     }
 }
